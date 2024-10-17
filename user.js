fa21067248b6346bb256beceb8f89ec1c1640821
wilkin <wilkinanthony94@gmail.com>
2024-10-17 18:27:54 -0500

    api course

--- user.js
+++ user.js
@@ -1,12 +1,20 @@
 const postListEl = document.querySelector('.post-list');
+const id = localStorage.getItem("id")
 
-async function main() {
-    const id = localStorage.getItem("id")
+async function onSearchChange(event) {
+    const id = event.target.value;
+    renderPosts(id);
+}
+
+async function renderPosts(id) {
     const posts = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${id}`)
     const postsData = await posts.json();
+    postListEl.innerHTML = postsData.map(post => postHTML(post)).join('');
+}
 
-    postListEl.innerHTML = postsData.map(post =>
-        `<div class="post">
+function postHTML(post) {
+    return `
+    <div class="post">
       <div class="post__title">
         ${post.title}
       </div>
@@ -14,7 +22,6 @@ async function main() {
         ${post.body}
       </p>
     </div>`
-    ).join('');
 }
 
-main();
+renderPosts(id);
